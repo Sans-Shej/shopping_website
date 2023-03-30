dad0f9aeaed2e39fa251c50f4d8ae708f8105452
Sans Shej <2003sans@gmail.com>
2023-03-30 22:37:47 +0530

    changes

--- Shopping-Website-master/src/main.js
+++ Shopping-Website-master/src/main.js
@@ -5,14 +5,14 @@ import store from "./store";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.css";
 
-// createApp(App).use(router).use(store).mount("#app");
-const base = axios.create({
-    baseURL: "http://localhost:8080"
-  });
+createApp(App).use(router).use(store).mount("#app");
+// const base = axios.create({
+//     baseURL: "http://localhost:8080"
+//   });
   
-  Vue.prototype.$http = base;
-  Vue.config.productionTip = false;
-  new Vue({
-    router,
-    render: h => h(App)
-  }).$mount("#app");
+//   Vue.prototype.$http = base;
+//   Vue.config.productionTip = false;
+//   new Vue({
+//     router,
+//     render: h => h(App)
+//   }).$mount("#app");
